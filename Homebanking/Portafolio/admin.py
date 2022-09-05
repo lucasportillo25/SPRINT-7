a64fa457d886faf2b52f464d1a3f2c3c2eff4b3e
lucasportillo25 <86985208+lucasportillo25@users.noreply.github.com>
2022-09-05 00:42:45 -0300

    admi

--- Homebanking/Portafolio/admin.py
+++ Homebanking/Portafolio/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+#Campos created y updated son de solo lectura
+class ProjectAdmin (admin.ModelAdmin):
+    readonly_fields= ('created','updated')
+admin.site.register(Proyectos)
