5412dd9e6def14d9a12de1ce8a23a02f135659ed
lucasportillo25 <86985208+lucasportillo25@users.noreply.github.com>
2022-09-05 01:06:23 -0300

    cliente

--- Homebanking/Clientes/views.py
+++ Homebanking/Clientes/views.py
@@ -5,4 +5,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def contact(resquest):
-    return render(request, 'contact.html')
+    return render(request, 'cliente.html')
